8cefa5cd25761a5f4b6f82c4ad752cf4da719828
Prathyusha Matta <146122912+9701716210@users.noreply.github.com>
2023-11-10 11:11:20 +0530

    second

---
+++ areaofcircle.cpp
@@ -0,0 +1,16 @@
+//area of circle
+#include<iostream>
+using namespace std;
+float area(int r,const float pi=3.14);
+int main()
+{
+	int r;
+	cout<<"Enter the radius:";
+	cin>>r;
+	cout<<"The area of the circle is:"<<area(r);
+	
+}
+float area(int r,const float pi)
+{
+	return pi*r*r;
+}
